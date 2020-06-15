b9bdaf24272c24405c2c1b6f2f32dd4b721a9af8
sadats90 <sadatshahriar.ss@gmail.com>
2020-06-15 21:42:10 +0600

    1

---
+++ assets/scripts/app.js
@@ -0,0 +1,20 @@
+alert('hello')
+const a = 0
+
+function add(a,b)
+{
+
+    alert(`result is ${a+b}`)
+
+}
+
+
+add(3,2)
+
+
+let b = a
+b = (a+10)*3/2-1
+c = '(' + a + ')' +'+10)*3/2-1'
+
+
+outputResult(b,c)
